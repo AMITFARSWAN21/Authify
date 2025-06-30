809f0b44f8ff7621f5cf6846db128f35184b8ccc
AMITFARSWAN21 <amitfarswan2104@gmail.com>
2025-06-30 13:25:56 +0530

    Add method to find all Marks by name in repository

--- src/main/java/com/authify/repository/MarksRepository.java
+++ src/main/java/com/authify/repository/MarksRepository.java
@@ -3,8 +3,11 @@ package com.authify.repository;
 import com.authify.entity.Marks;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface MarksRepository extends JpaRepository<Marks,Long> {
     Optional<Marks> findByName(String name);
+    List<Marks> findAllByName(String name);
+
 }
